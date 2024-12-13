d591d179dfc65a0c4070880800d38dd90239828a
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2024-12-13 05:09:05 -0500

    CRUD - Personal

--- Aplicaciones/Cliente/urls.py
+++ Aplicaciones/Cliente/urls.py
@@ -5,7 +5,14 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('listar_ciudad/', views.listar_ciudad, name='listar_ciudad'),
     path('insertar_ciudad/', views.insertar_ciudad, name='insertar_ciudad'),
-    path('obtener_ciudad/<int:id>/', views.obtener_ciudad, name='obtener_ciudad'),
+    path('obtener_ciudad/<id>/', views.obtener_ciudad, name='obtener_ciudad'),
     path('actualizar_ciudad/', views.actualizar_ciudad, name='actualizar_ciudad'),
-    path('eliminar_ciudad/<int:id>/', views.eliminar_ciudad, name='eliminar_ciudad'),
+    path('eliminar_ciudad/<id>/', views.eliminar_ciudad, name='eliminar_ciudad'),
+    path('listar_propietario/', views.listar_propietario, name='listar_propietario'),
+    path('insertar_propietario/', views.insertar_propietario, name='insertar_propietario'),
+    path('obtener_propietario/<id>/', views.obtener_propietario, name='obtener_propietario'),
+    path('actualizar_prpietario/', views.actualizar_prpietario, name='actualizar_prpietario'),
+    path('eliminar_propietario/<id>', views.eliminar_propietario, name='eliminar_propietario'),
+    
+    
 ]
--- Aplicaciones/Cliente/views.py
+++ Aplicaciones/Cliente/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Ciudad, Propietario
@@ -13,6 +14,7 @@ def listar_ciudad(request):
 def insertar_ciudad(request):
     nombre = request.POST['nombre'].upper()
     nueva_ciudad = Ciudad.objects.create(nombre_ciu = nombre)
+    messages.success(request, "Ciudad registrado correctamente.")
     return redirect('listar_ciudad')
 
 def obtener_ciudad(request, id):
@@ -30,9 +32,69 @@ def actualizar_ciudad(request):
     ciudad = get_object_or_404(Ciudad, id_ciu=id)
     ciudad.nombre_ciu = nombre
     ciudad.save()
+    messages.success(request, "Ciudad actualizado correctamente.")
     return redirect('listar_ciudad')
 
 def eliminar_ciudad(request, id):
     ciudad = get_object_or_404(Ciudad, id_ciu=id)
     ciudad.delete()
+    messages.success(request, "Ciudad eliminado correctamente.")
     return redirect('listar_ciudad')
+
+def listar_propietario(request):
+    getPropietario = Propietario.objects.all()
+    getCiudad = Ciudad.objects.all()
+    return render(request, 'Propietario/listar_propietario.html', { 'propietario':getPropietario, 'ciudad':getCiudad })
+
+def insertar_propietario(request):
+    nombre = request.POST.get('nombre').upper()
+    apellido = request.POST.get('apellido').upper()
+    correo = request.POST.get('correo')
+    telefono = request.POST.get('telefono')
+    ciudad_id = request.POST.get('ciudad')
+    
+    ciudad = get_object_or_404(Ciudad, id_ciu=ciudad_id)
+    
+    nuevo_Propietario = Propietario.objects.create(nombre_pro = nombre, apellido_pro = apellido, email_pro = correo, telefono_pro = telefono, fkid_ciu = ciudad)
+    
+    messages.success(request, "Personal registrado correctamente.")
+    return redirect('listar_propietario')
+
+def obtener_propietario(request, id):
+    propietario = get_object_or_404(Propietario, id_pro=id)
+    data = {
+        'id_pro': propietario.id_pro,
+        'nombre_pro': propietario.nombre_pro,
+        'apellido_pro': propietario.apellido_pro,
+        'email_pro': propietario.email_pro,
+        'telefono_pro': propietario.telefono_pro,
+        'fkid_ciu': propietario.fkid_ciu.id_ciu,
+    }
+    return JsonResponse(data)
+
+def actualizar_prpietario(request):
+    id = request.POST.get('id_act')
+    nombre = request.POST.get('nombre_act').upper()
+    apellido = request.POST.get('apellido_act').upper()
+    correo = request.POST.get('correo_act')
+    telefono = request.POST.get('telefono_act')
+    ciudad_id = request.POST.get('ciudad_act')
+    
+    ciudad = get_object_or_404(Ciudad, id_ciu=ciudad_id)
+    
+    propietario = get_object_or_404(Propietario, id_pro=id)
+    propietario.nombre_pro = nombre
+    propietario.apellido_pro = apellido
+    propietario.email_pro = correo
+    propietario.telefono_pro = telefono
+    propietario.fkid_ciu = ciudad
+    propietario.save()
+    
+    messages.success(request, "Personal actualizado correctamente.")
+    return redirect('listar_propietario')
+
+def eliminar_propietario(request, id):
+    propietario = get_object_or_404(Propietario, id_pro=id)
+    propietario.delete()
+    messages.success(request, "Personal eliminado correctamente.")
+    return redirect('listar_propietario')
