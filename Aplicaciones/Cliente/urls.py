c265ed574404c4eb8398d854d925d6085fb33ead
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2024-12-12 21:21:15 -0500

    creacion de la vista listar vehiculos

--- Aplicaciones/Cliente/urls.py
+++ Aplicaciones/Cliente/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('listar_propietario/', views.listar_propietario, name='listar_propietario'),
 ]
--- Aplicaciones/Cliente/views.py
+++ Aplicaciones/Cliente/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def listar_propietario(request):
+    return render(request, 'Propietarios/listar_propieretario.html')
